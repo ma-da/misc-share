1fb5c60f7cae066396c7f1e0a795a1312fc029ed
ma-da <32108703+ma-da@users.noreply.github.com>
2018-04-02 16:09:10 -0500

    Update parseWTKnews.py

--- parseWTKnews.py
+++ parseWTKnews.py
@@ -22,10 +22,10 @@ Story = []
 for i in range(0, len(Raw_stories), 6):
     Story.append(Raw_stories[i : i+6])
 '''
+#initialize some lists
+
 Headlines = []
 Datesources = []
-Dates = []
-News_sources = []
 News_source_links = []
 Quotes = []
 Summaries = []
@@ -33,11 +33,15 @@ Note_text = []
 Adtl_ref_links = []
 Note_ref_links = []
 
+#create other lists from beautifulsoup oblects
+
 titles_list = Raw_stories[0::6]
 date_sources = Raw_stories[1::6]
 story_links = Raw_stories[2::6]
 raw_sums = Raw_stories[4::6]
 notes = Raw_stories[5::6]
+
+#simple loops make columns
 
 for i in titles_list:
     headline = i.text
@@ -51,6 +55,8 @@ for i in story_links:
     story_link = i.a
     News_source_links.append(story_link)
 
+#these loops extract important stuff from summmaries and summary notes	
+	
 for i in raw_sums:
     try:
         quote = i.strong.text
@@ -71,6 +77,8 @@ for i in notes:
     note_links.append(nlinks)
     Note_text.append(note)
     Note_ref_links.append(note_links)
+
+#make initial dataframe from populated columns	
     
 df = pd.DataFrame({'Headline': Headlines,
                   'Datesource': Datesources,
@@ -81,14 +89,15 @@ df = pd.DataFrame({'Headline': Headlines,
                   'Summary_ref_links': Adtl_ref_links,
                   'Note_ref_links': Note_ref_links})
 
+#check to make sure it looks right				  
 df.head(5)
 
-# Split delimited values in DataFrame 'Datesource' column into new 'Date' and 'News_source' columns
+#split delimited values in DataFrame 'Datesource' column into new 'Date' and 'News_source' columns
 df['Date'], df['News_source'] = df['Datesource'].str.split(' ', 1).str
 
-# Drops unused datesource column
+#drops now-unused datesource column
 df.drop('Datesource', axis=1, inplace=True)
 
 #checks that all columns are present. Object dtypes easily changed as needed.
-df.dtypes	
+df.dtypes
 
